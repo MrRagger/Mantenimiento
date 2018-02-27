28cae5a3bfc3b3ebf59d76df278db1562a7c5c6e
mrodriguez2 <manuelrodriguezrodriguez27@gmail.com>
2018-02-27 21:40:55 +0100

    Add files via upload

--- panel/tren.php
+++ panel/tren.php
@@ -28,7 +28,7 @@ $resultado = $conexion->query($sql);
 
 <body>
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">Tren</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">Mantenimiento</a>
     <ul class="navbar-nav px-3">
       <li class="nav-item text-nowrap">
         <a class="nav-link" href="login.php">Desconectar</a>
