372da2ad9cddbd414fbbe995551bfc29e526cdb2
Ineshs3 <36666011+Ineshs3@users.noreply.github.com>
2018-04-21 18:54:00 +0200

    Update Algoritmo.java

--- simulated.device.app/src/main/java/com/mycompany/app/simulated/device/app/Algoritmo.java
+++ simulated.device.app/src/main/java/com/mycompany/app/simulated/device/app/Algoritmo.java
@@ -1,23 +1,126 @@
 package com.mycompany.app.simulated.device.app;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import com.mysql.jdbc.Driver;
+
+import java.sql.*;
+
+
 
 import com.mycompany.app.simulated.device.app.App.MessageSender;
 
 
 
 public class Algoritmo {
-	private Connection connection;
+	private Connection connection=null;
+	String serverAddress = "77.104.160.115";
+	String db = "onlyf1st_mantenimiento";
+	String user = "onlyf1st_root";
+	String pass = "mantenimiento";
+	String url = "jdbc:mysql://" + serverAddress + "/" + db;
 	public Algoritmo() {
 		try {
-			conectar();
+		Class.forName("com.mysql.jdbc.Driver");
+		connection=(Connection)DriverManager.getConnection(url, user, pass);
+		if(connection!=null) {
+			System.out.println("Conectado a la base de datos!");
+		}
+		}
+		catch(SQLException e) {
+			System.out.println("No se ha podido conectar a la BD");
+			
+		}
+		catch(ClassNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+
+	public void exit() throws SQLException {
+		System.out.println("Saliendo.. ï¿œhasta otra!");
+		connection.close();
+		System.exit(0);
+	}
+
+	public void conectar() throws SQLException, ClassNotFoundException {
+
+ 
+		try {
+			//new MessageSender().run(); 
+			Statement st2 = connection.createStatement(); //Creamos un statement con la conexiï¿œn
+			ResultSet rs2 = st2.executeQuery("SELECT Sensor.IdPieza, Sensor.IdSensor, Sensor.Color FROM Sensor");//Formamos la query con la consulta que queremos hacer
+			
+			while (rs2.next()) { //Bucle para ir avanzando en la query
+				String id = rs2.getString("Sensor.IdSensor"); //Sacamos el id del sensor
+				String pieza = rs2.getString("Sensor.IdPieza"); //Sacamos la pieza donde esta el sensor
+				String color = rs2.getString("Sensor.Color"); //Sacamos el color del sensor
+
+				new MessageSender().run(); //Medido en %
+				double humedad = 0;
+				double temperature=0;
+				//Medido en kg/cm2
+				//System.out.println(temperature);
+				if(pieza=="Temperature") {
+					if ((7 <= temperature) && (temperature<= 10)){
+						ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Verde' WHERE IdSensor=id AND IdPieza='pieza'");
+						
+						colorSensor.close();
+					}
+
+					else { if(((10 < temperature)&&  (temperature< 15)) || ((2 < temperature)&&(temperature < 7))) { 
+
+						ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Naranja' WHERE IdSensor=id AND IdPieza='pieza'");
+						
+						colorSensor.close();
+					}
+					else {
+						ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Rojo' WHERE IdSensor=id AND IdPieza='pieza'");
+						colorSensor.close();
+					}
+					}
+				}
+				if(pieza=="Humedad") {
+					if ((40 < humedad )&&(humedad<= 60) ){
+						ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Verde' WHERE IdSensor=id AND IdPieza='pieza'");
+						colorSensor.close();
+					}
+					else {
+						if( (60 < humedad)&& (humedad < 75)) {
+
+							ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Naranja' WHERE IdSensor=id AND IdPieza='pieza'");
+							colorSensor.close();
+						}
+						else {
+							ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Rojo' WHERE IdSensor=id AND IdPieza='pieza'");
+
+							colorSensor.close();
+						}
+
+					}
+
+				}
+
+				
+			}
+			
+			st2.close();
+		rs2.close();
+
+
+		}  catch (SQLException e) {
+			System.out.println("No se pudo realizar la operacion");
+			System.out.println("Mensaje de error: " + e.getMessage());
+			System.out.println("Codigo de error: " + e.getErrorCode());
+			System.out.println("Estado SQL: " + e.getSQLState());
+		} 
+
+
+          
+	}	
+
+	public static void main(String args[]) {
+		Algoritmo algoritmos= new Algoritmo();
+		
+		
+		try {
+			algoritmos.conectar();
+			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -25,67 +128,8 @@ public class Algoritmo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	private void exit() throws SQLException {
-		System.out.println("Saliendo.. �hasta otra!");
-		connection.close();
-		System.exit(0);
-	}
-	private void conectar() throws SQLException, ClassNotFoundException {
-		String drv = "com.mysql.jdbc.Driver";
-		Class.forName(drv);
+		
 
-		String serverAddress = "77.104.160.115";
-		String db = "onlyf1st_mantenimiento";
-		String user = "onlyf1st_root";
-		String pass = "mantenimiento";
-		String url = "jdbc:mysql://" + serverAddress + "/" + db;
-		connection = DriverManager.getConnection(url, user, pass);
-		System.out.println("Conectado a la base de datos!");
 	}
-	void analizarDatos() throws SQLException {
-		Statement st2 = connection.createStatement(); //Creamos un statement con la conexi�n
-		ResultSet rs2 = st2.executeQuery("SELECT Sensor.IdPieza, Sensor.IdSensor, Sensor.Color from Sensor;");//Formamos la query con la consulta que queremos hacer
-		while (rs2.next()) { //Bucle para ir avanzando en la query
-			String id = rs2.getString("Sensor.IdSensor"); //Sacamos el id del sensor
-			String pieza = rs2.getString("Sensor.IdPieza"); //Sacamos la pieza donde esta el sensor
-			//String color = rs2.getString("Sensor.Color"); //Sacamos el color del sensor
-               //System.out.println(id);
-			double humedad=MessageSender.sensor.getHumidity(); //Medido en %
-			System.out.println(humedad);
-			double temperature=MessageSender.sensor.getTemperature(); //Medido en kg/cm2
 
-		if(pieza=="Temperature") {
-				if ((7 <= temperature) && (temperature<= 10)){
-					ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Verde' WHERE IdSensor=id AND IdPieza='pieza';");
-				System.out.println(colorSensor);
-				}
-				else {
-					   if(((10 < temperature)&&  (temperature< 15)) || ((2 < temperature)&&(temperature < 7))) { 
-				
-						   ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Naranja' WHERE IdSensor=id AND IdPieza='pieza';");
-						   System.out.println(colorSensor);
-					   }
-				       else {
-				    	   ResultSet colorSensor=st2.executeQuery("UPDATE Sensor SET Color='Rojo' WHERE IdSensor=id AND IdPieza='pieza';");
-				     }
-				}
-			}
-			if(pieza=="Humedad") {
-				if ((40 < humedad )&&(humedad<= 60) ){
-					ResultSet s1=st2.executeQuery("UPDATE Sensor SET Color='Verde' WHERE IdSensor=id AND IdPieza='pieza';");
-				}
-				else {
-					  if( (60 < humedad)&& (humedad < 75)) {
-				
-						  ResultSet s1=st2.executeQuery("UPDATE Sensor SET Color='Naranja' WHERE IdSensor=id AND IdPieza='pieza';");
-					  }
-				      else {
-				    	  ResultSet s1=st2.executeQuery("UPDATE Sensor SET Color='Rojo' WHERE IdSensor=id AND IdPieza='pieza';");
-				      }
-		}
-		}
-		}
-		}
-}
-			
+}		
